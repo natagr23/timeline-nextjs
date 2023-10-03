f91bfa73e89f09b293d6ff12e6dc403c78103c6d
natagr23 <nata_garcia23@hotmail.com>
2023-10-03 09:24:37 -0500

    Fix hydratation error using Head from Next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import '@aws-amplify/ui-react/styles.css';
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
 import { getMetadata } from './metadata';
+import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
@@ -12,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="App">
-        <head>
+        <Head>
           <title>{title.default}</title>
-        </head>
+        </Head>
         <Navbar />
         {children}
         <Footer />
